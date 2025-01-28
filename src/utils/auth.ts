20dfeed3e1fd7880dcaa367863b8c8b6bbcd64dd
wiyco <72733890+wiyco@users.noreply.github.com>
2025-01-29 07:18:05 +0900

    delete(utils): 不要なモジュールを削除

--- src/utils/auth.ts
+++
@@ -1,6 +0,0 @@
-import type { Context } from "hono";
-import type { Bindings } from "~/types";
-
-export function isValidRequest(c: Context<{ Bindings: Bindings }>): boolean {
-  return c.req.header("XXX-API-ENDPOINT-SECRET") === c.env.API_ENDPOINT_SECRET;
-}
