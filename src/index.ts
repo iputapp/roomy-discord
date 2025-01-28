601625edb4c6f162768dca22ca1b228485ee96e7
wiyco <72733890+wiyco@users.noreply.github.com>
2025-01-29 06:57:04 +0900

    feat(app): handler, middlewareを利用
    関連
    auth-middleware: 0eb12a48a71f12708e93f06405c211623error-handler: b7e8513
    41935d5a17c9d0c75167b3e1cb44aed7408eaaf6

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { Hono } from "hono";
 import api from "~/api";
+import { APIError, apiErrorHandler } from "~/handler";
+import { authMiddleware } from "~/middleware";
 import type { Bindings } from "~/types";
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+app.notFound((c) =>
+  apiErrorHandler(new APIError("Not found or Method not allowed", 404), c),
+);
+
+app.onError(apiErrorHandler);
+
+app.use("/v1/cron/*", authMiddleware);
+app.use("/v1/google/*", authMiddleware);
 
 app.route("/", api);
 
