96d0ce12b4617067c1bd54c682f68e922a45a895
wiyco <72733890+wiyco@users.noreply.github.com>
2025-01-29 08:43:37 +0900

    feat(app): scheduleを登録

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import api from "~/api";
 import { APIError, apiErrorHandler } from "~/handler";
 import { authMiddleware } from "~/middleware";
 import type { Bindings } from "~/types";
+import { callCronEndpoint } from "./schedule";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
@@ -17,4 +18,20 @@ app.use("/v1/google/*", authMiddleware);
 
 app.route("/", api);
 
-export default app;
+const scheduled: ExportedHandlerScheduledHandler<Bindings> = async (
+  _ctrl,
+  env,
+  ctx,
+) => {
+  ctx.waitUntil(
+    callCronEndpoint({
+      path: "/v1/cron",
+      env,
+    }),
+  );
+};
+
+export default {
+  fetch: app.fetch,
+  scheduled,
+};
