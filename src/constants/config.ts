d4aec0e37321bfedd6abfcdc627a3180f74abcd3
wiyco <72733890+wiyco@users.noreply.github.com>
2025-02-05 11:51:17 +0900

    refactor(constants): キーの順序を変更

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -9,11 +9,11 @@ export const DISCORD_API_BASE_URL = "https://discord.com/api/v10";
 export function parseEnv(env: Bindings) {
   return {
     discord: {
+      apiBaseUrl: DISCORD_API_BASE_URL,
       token: env.DISCORD_TOKEN || "",
       applicationId: env.DISCORD_APPLICATION_ID || "",
       publicKey: env.DISCORD_PUBLIC_KEY || "",
       channelId: env.DISCORD_CHANNEL_ID || "",
-      apiBaseUrl: DISCORD_API_BASE_URL,
     },
     googleSheets: {
       clientEmail: env.GOOGLE_CLIENT_EMAIL || "",
