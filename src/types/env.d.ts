7a8b8d00b4bb01ca8929bbfe70ed31f2a90a0216
wiyco <72733890+wiyco@users.noreply.github.com>
2025-02-05 11:42:00 +0900

    feat(types): node envに環境を追加
    関連 73775a7ca8271513dc71575a65c21f6dcec08b42

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,5 @@
 export interface Bindings {
-  NODE_ENV: "development" | "production" | "test";
+  NODE_ENV: "development" | "staging" | "production" | "test";
   DISCORD_TOKEN: string;
   DISCORD_APPLICATION_ID: string;
   DISCORD_PUBLIC_KEY: string;
